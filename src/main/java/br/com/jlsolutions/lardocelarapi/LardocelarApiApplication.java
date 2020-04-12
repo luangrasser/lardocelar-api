bef8bea3ca3772e8c7914b82ce296184400918a7
luangrasser <bonilhaluan@gmail.com>
2020-04-12 18:51:25 -0300

    initial commit

---
+++ src/main/java/br/com/jlsolutions/lardocelarapi/LardocelarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jlsolutions.lardocelarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LardocelarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LardocelarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jlsolutions/lardocelarapi/LardocelarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jlsolutions.lardocelarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LardocelarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
